965c6da78ee90800cea023010a9d764b1b0a4b18
jo_mut <johnmutuku628@gmail.com>
2019-09-06 16:06:54 +0300

    configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkerComponent } from './workers/worker/worker.component';
+import { PropertiesListComponent } from './properties/properties-list/properties-list.component';
+import { WorkersListComponent } from './workers/workers-list/workers-list.component';
+import { CamerasListComponent } from './cameras/cameras-list/cameras-list.component';
+import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
 
 const routes: Routes = [
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent},
+    {path: 'workers', component: WorkersListComponent},
+    {path: 'propeties', component: PropertiesListComponent},
+    {path: 'cameras', component: CamerasListComponent},
+    {path: 'projects', component: ProjectsListComponent},
+    {path: '**', redirectTo: "dashboard"},
 
 ];
 
@@ -14,4 +24,7 @@ const routes: Routes = [
   })
 
   export class AppRoutingModule { }
-  export const RoutingComponents = [DashboardComponent]
+  export const RoutingComponents = [
+        DashboardComponent, WorkersListComponent,
+        CamerasListComponent, ProjectsListComponent,
+        PropertiesListComponent,];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,40 +3,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainPanelComponent } from './components/main-panel/main-panel.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { WorkersListComponent } from './workers/workers-list/workers-list.component';
 import { WorkerComponent } from './workers/worker/worker.component';
 import { ProfileComponent } from './profile/profile/profile.component';
 import { CameraComponent } from './cameras/camera/camera.component';
-import { CamerasListComponent } from './cameras/cameras-list/cameras-list.component';
-import { PropertiesListComponent } from './properties/properties-list/properties-list.component';
 import { PropertyComponent } from './properties/property/property.component';
 import { ProjectComponent } from './projects/project/project.component';
-import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent,
     SidebarComponent,
     NavbarComponent,
     MainPanelComponent,
     FooterComponent,
     RoutingComponents,
-    WorkersListComponent,
     WorkerComponent,
     ProfileComponent,
     CameraComponent,
-    CamerasListComponent,
-    PropertiesListComponent,
     PropertyComponent,
     ProjectComponent,
-    ProjectsListComponent
   ],
   imports: [
     BrowserModule,
